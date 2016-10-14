e3fabfa4e795dd3a561b813284e78ac4ef1b3d34
Marc Clifton <marc.clifton@gmail.com>
2016-10-14 16:09:34 -0400

    Updated article.

--- FlowSharpLib/MouseController.cs
+++ FlowSharpLib/MouseController.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/* 
+* Copyright (c) Marc Clifton
+* The Code Project Open License (CPOL) 1.02
+* http://www.codeproject.com/info/cpol10.aspx
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;

130f6d8ed24826e9db334e3913a93a7d32193468
Marc Clifton <marc.clifton@gmail.com>
2016-11-17 08:02:05 -0500

    Moved copy/paste/delete to Operations.cs so it's more of a re-usable subset.

--- FlowSharpUI.cs
+++ FlowSharpUI.cs
@@ -8,9 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
-using System.IO;
 using System.Linq;
-using System.Reflection;
 using System.Windows.Forms;
 
 using FlowSharpLib;
@@ -189,160 +187,6 @@ namespace FlowSharp
             }
 
             return ret;
-        }
-
-        protected void Copy()
-        {
-            if (canvasController.SelectedElements.Any())
-            {
-                List<GraphicElement> elementsToCopy = new List<GraphicElement>();
-                // Include child elements of any groupbox, otherwise, on deserialization,
-                // the ID's for the child elements aren't found.
-                elementsToCopy.AddRange(canvasController.SelectedElements);
-                elementsToCopy.AddRange(IncludeChildren(elementsToCopy));
-                string copyBuffer = Persist.Serialize(elementsToCopy.OrderByDescending(el => canvasController.Elements.IndexOf(el)));
-                Clipboard.SetData("FlowSharp", copyBuffer);
-            }
-            else
-            {
-                MessageBox.Show("Please select one or more shape(s).", "Nothing to copy.", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            }
-        }
-
-        protected List<GraphicElement> IncludeChildren(List<GraphicElement> parents)
-        {
-            List<GraphicElement> els = new List<GraphicElement>();
-
-            parents.ForEach(p =>
-            {
-                els.AddRange(p.GroupChildren);
-                els.AddRange(IncludeChildren(p.GroupChildren));
-            });
-
-            return els;
-        }
-
-        protected void Paste()
-        {
-            string copyBuffer = Clipboard.GetData("FlowSharp")?.ToString();
-
-            if (copyBuffer == null)
-            {
-                MessageBox.Show("Clipboard does not contain a FlowSharp shape", "Paste Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-            else
-            {
-                try
-                {
-                    List<GraphicElement> els = Persist.Deserialize(canvas, copyBuffer);
-                    List<GraphicElement> selectedElements = canvasController.SelectedElements.ToList();
-
-                    // After deserialization, only move and select elements without parents -
-                    // children of group boxes should not be moved, as their parent will handle this,
-                    // and children of group boxes cannot be selected.
-                    List<GraphicElement> noParentElements = els.Where(e => e.Parent == null).ToList();
-
-                    noParentElements.ForEach(el =>
-                    {
-                        el.Move(new Point(20, 20));
-                        el.UpdateProperties();
-                        el.UpdatePath();
-                    });
-
-                    List<GraphicElement> intersections = new List<GraphicElement>();
-
-                    els.ForEach(el =>
-                    {
-                        intersections.AddRange(canvasController.FindAllIntersections(el));
-                    });
-
-                    IEnumerable<GraphicElement> distinctIntersections = intersections.Distinct();
-
-                    canvasController.UndoStack.UndoRedo("Paste",
-                        () =>
-                        {
-                            canvasController.DeselectCurrentSelectedElements();
-
-                            canvasController.EraseTopToBottom(distinctIntersections);
-
-                            els.ForEach(el =>
-                            {
-                                canvasController.Insert(0, el);
-                                ElementCache.Instance.Remove(el);
-                            });
-
-                            canvasController.DrawBottomToTop(distinctIntersections);
-                            canvasController.UpdateScreen(distinctIntersections);
-                            noParentElements.ForEach(el => canvasController.SelectElement(el));
-                        }
-                        ,
-                        () =>
-                        {
-                            canvasController.DeselectCurrentSelectedElements();
-
-                            els.ForEach(el =>
-                            {
-                                canvasController.DeleteElement(el, false);
-                                ElementCache.Instance.Add(el);
-                            });
-
-                            canvasController.SelectElements(selectedElements);
-                        });
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show("Error pasting shape:\r\n" + ex.Message, "Paste Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
-            }
-        }
-
-        protected void Delete()
-        {
-            if (canvasController.Canvas.Focused)
-            {
-                List<ZOrderMap> originalZOrder = canvasController.GetZOrderOfSelectedElements();
-                List<GraphicElement> selectedElements = canvasController.SelectedElements.ToList();
-
-                // TODO: Better implementation would be for the mouse controller to hook a shape deleted event?
-                canvasController.SelectedElements.ForEach(el => mouseController.ShapeDeleted(el));
-
-                canvasController.UndoStack.UndoRedo("Delete",
-                    () =>
-                    {
-                        canvasController.DeleteSelectedElementsHierarchy(false);
-                    },
-                    () =>
-                    {
-                        canvasController.RestoreZOrderWithHierarchy(originalZOrder);
-                        RestoreConnections(originalZOrder);
-                        canvasController.DeselectCurrentSelectedElements();
-                        canvasController.SelectElements(selectedElements);
-                    });
-            }
-        }
-
-        protected void RestoreConnections(List<ZOrderMap> zomList)
-        {
-            foreach (ZOrderMap zom in zomList)
-            {
-                GraphicElement el = zom.Element;
-                List<Connection> connections = zom.Connections;
-
-                foreach (Connection conn in connections)
-                {
-                    conn.ToElement.SetConnection(conn.ToConnectionPoint.Type, el);
-                }
-            }
-        }
-
-        protected void Undo()
-        {
-            canvasController.Undo();
-        }
-
-        protected void Redo()
-        {
-            canvasController.Redo();
         }
 
         protected bool CanStartEditing(Keys keyData)
@@ -540,11 +384,6 @@ namespace FlowSharp
         {
             toolboxController.Elements.ForEach(el => el.UpdatePath());
         }
-
-        protected void UpdateCaption()
-		{
-			Text = "FlowSharp" + (String.IsNullOrEmpty(filename) ? "" : " - ") + filename;
-		}
 
         private void mnuDebugWindow_Click(object sender, EventArgs e)
         {

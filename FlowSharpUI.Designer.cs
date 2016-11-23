fdd4c10a7c0cfd06908335c67515b5cb39fb7a43
Marc Clifton <marc.clifton@gmail.com>
2016-11-23 18:00:21 -0500

    Dispose TraceListener instance.

--- FlowSharpUI.Designer.cs
+++ FlowSharpUI.Designer.cs
@@ -16,6 +16,7 @@
             if (disposing && (components != null))
             {
                 components.Dispose();
+                traceListener.Dispose();
             }
             base.Dispose(disposing);
         }

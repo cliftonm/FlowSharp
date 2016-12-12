fa46ddd9302036dcbe6ce1db4d6d2b3d09f17cd4
Marc Clifton <marc.clifton@gmail.com>
2016-12-11 21:05:46 -0500

    Example of adding WinForm controls to canvas.

---
+++ Plugins/PluginExample/ButtonShape.cs
@@ -0,0 +1,33 @@
+﻿/* 
+* Copyright (c) Marc Clifton
+* The Code Project Open License (CPOL) 1.02
+* http://www.codeproject.com/info/cpol10.aspx
+*/
+
+using System.Drawing;
+using System.Windows.Forms;
+
+using FlowSharpLib;
+
+namespace PluginExample
+{
+    public class ButtonShape : Box
+    {
+        protected Button button;
+
+        public ButtonShape(Canvas canvas) : base(canvas)
+        {
+            button = new Button();
+            canvas.Controls.Add(button);
+        }
+
+        public override void Draw(Graphics gr)
+        {
+            base.Draw(gr);
+            Rectangle r = DisplayRectangle.Grow(-4);
+            button.Location = r.Location;
+            button.Size = r.Size;
+            button.Text = Text;
+        }
+    }
+}
---
+++ Plugins/PluginExample/CheckboxShape.cs
@@ -0,0 +1,33 @@
+﻿/* 
+* Copyright (c) Marc Clifton
+* The Code Project Open License (CPOL) 1.02
+* http://www.codeproject.com/info/cpol10.aspx
+*/
+
+using System.Drawing;
+using System.Windows.Forms;
+
+using FlowSharpLib;
+
+namespace PluginExample
+{
+    public class CheckboxShape : Box
+    {
+        protected CheckBox checkbox;
+
+        public CheckboxShape(Canvas canvas) : base(canvas)
+        {
+            checkbox = new CheckBox();
+            canvas.Controls.Add(checkbox);
+        }
+
+        public override void Draw(Graphics gr)
+        {
+            base.Draw(gr);
+            Rectangle r = DisplayRectangle.Grow(-4);
+            checkbox.Location = r.Location;
+            checkbox.Size = r.Size;
+            checkbox.Text = Text;
+        }
+    }
+}

38958312843435df24c5b1fa1a69ab8e0efc3b2e
Marc Clifton <marc.clifton@gmail.com>
2017-02-14 12:08:36 -0500

    DRAKON "if" box.

---
+++ FlowSharpLib/Shapes/AngleBracketBox.cs
@@ -0,0 +1,91 @@
+﻿/* 
+* Copyright (c) Marc Clifton
+* The Code Project Open License (CPOL) 1.02
+* http://www.codeproject.com/info/cpol10.aspx
+*/
+
+using System.Drawing;
+
+namespace FlowSharpLib
+{
+    [ExcludeFromToolbox]
+    public class AngleBracketBox : GraphicElement
+    {
+        protected Point[] path;
+        protected const int INDENT_SIZE = 12;
+
+        public AngleBracketBox(Canvas canvas) : base(canvas)
+        {
+            HasCornerConnections = false;
+        }
+
+        public override void UpdatePath()
+        {
+            path = new Point[]
+            {
+                new Point(DisplayRectangle.X + INDENT_SIZE, DisplayRectangle.Y),                                                            // top left of indented left "arrow"
+                new Point(DisplayRectangle.X + DisplayRectangle.Width - INDENT_SIZE,    DisplayRectangle.Y),                                // top right of indented right "arrow"
+                new Point(DisplayRectangle.X + DisplayRectangle.Width, DisplayRectangle.Y + DisplayRectangle.Height/2),                     // right tip (middle of box)
+                new Point(DisplayRectangle.X + DisplayRectangle.Width - INDENT_SIZE, DisplayRectangle.Y + DisplayRectangle.Height),         // bottom right of indented right "arrow"
+                new Point(DisplayRectangle.X + INDENT_SIZE, DisplayRectangle.Y + DisplayRectangle.Height),                                  // bottom left of indented left "arrow"
+                new Point(DisplayRectangle.X, DisplayRectangle.Y + DisplayRectangle.Height/2),                                                            // middle left of indented left "arrow"
+            };
+        }
+
+        public override void Draw(Graphics gr)
+        {
+            gr.FillPolygon(FillBrush, path);
+            gr.DrawPolygon(BorderPen, path);
+            base.Draw(gr);
+        }
+    }
+
+    [ToolboxShape]
+    [ToolboxOrder(4)]
+    public class ToolboxAngleBracketBox : GraphicElement
+    {
+        protected Point[] path;
+        protected const int INDENT_SIZE = 5;
+        protected const int V_ADJ = 3;
+
+        public ToolboxAngleBracketBox(Canvas canvas) : base(canvas)
+        {
+            HasCornerConnections = false;
+        }
+
+        public override GraphicElement CloneDefault(Canvas canvas)
+        {
+            return CloneDefault(canvas, Point.Empty);
+        }
+
+        public override GraphicElement CloneDefault(Canvas canvas, Point offset)
+        {
+            AngleBracketBox shape = new AngleBracketBox(canvas);
+            shape.DisplayRectangle = shape.DefaultRectangle().Move(offset);
+            shape.UpdateProperties();
+            shape.UpdatePath();
+
+            return shape;
+        }
+
+        public override void UpdatePath()
+        {
+            path = new Point[]
+            {
+                new Point(DisplayRectangle.X + INDENT_SIZE, DisplayRectangle.Y + V_ADJ),                                                                  // top left of indented left "arrow"
+                new Point(DisplayRectangle.X + DisplayRectangle.Width - INDENT_SIZE, DisplayRectangle.Y + V_ADJ),                                         // top right of indented right "arrow"
+                new Point(DisplayRectangle.X + DisplayRectangle.Width, DisplayRectangle.Y + DisplayRectangle.Height/2),                                   // right tip (middle of box)
+                new Point(DisplayRectangle.X + DisplayRectangle.Width - INDENT_SIZE, DisplayRectangle.Y + DisplayRectangle.Height - V_ADJ),               // bottom right of indented right "arrow"
+                new Point(DisplayRectangle.X + INDENT_SIZE, DisplayRectangle.Y + DisplayRectangle.Height - V_ADJ),                                        // bottom left of indented left "arrow"
+                new Point(DisplayRectangle.X, DisplayRectangle.Y + DisplayRectangle.Height/2),                                                            // middle left of indented left "arrow"
+            };
+        }
+
+        public override void Draw(Graphics gr)
+        {
+            gr.FillPolygon(FillBrush, path);
+            gr.DrawPolygon(BorderPen, path);
+            base.Draw(gr);
+        }
+    }
+}
